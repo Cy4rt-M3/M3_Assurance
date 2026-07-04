7964be19f407b0bf65c80c4ad309355319c64c56
Rishi Gaikwad <rishigaikwad715@gmail.com>
2026-07-04 16:29:56 +0530

    Add Python CLI calculator

---
+++ apps/Calculator.py
@@ -0,0 +1,18 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+op = input("Enter operation (+, -, *, /): ")
+
+if op == "+":
+    print("Answer:", num1 + num2)
+elif op == "-":
+    print("Answer:", num1 - num2)
+elif op == "*":
+    print("Answer:", num1 * num2)
+elif op == "/":
+    if num2 != 0:
+        print("Answer:", num1 / num2)
+    else:
+        print("Cannot divide by zero")
+else:
+    print("Invalid operation")
